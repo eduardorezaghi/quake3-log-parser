9e760223ca4056eb72e2919b0408ca8694bf6507
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-07-20 13:46:16 -0300

    Add argparse option to group deaths by means

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
-import json
 import argparse
+import json
 from pathlib import Path
+
 from src.dclasses import QuakeLogEncoder
 from src.parser import QuakeLogParser
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Parse a Quake log file.")
-    parser.add_argument('log_file', type=str, help='Path to the Quake log file')
+    parser.add_argument("log_file", type=str, help="Path to the Quake log file")
+    parser.add_argument(
+        "--group-deaths", action="store_true", help="Group deaths by means"
+    )
     return parser.parse_args()
+
 
 def run(args):
     log_file_path = Path(args.log_file)
@@ -16,9 +22,15 @@ def run(args):
         raise FileNotFoundError(f"Error: The file {log_file_path} does not exist.")
 
     parser = QuakeLogParser(log_file_path)
+
     parser.parse()
-    print(json.dumps(parser.games, indent=4, cls=QuakeLogEncoder))
+    if args.group_deaths:
+        output = parser._group_deaths_by_means()
+    else:
+        output = parser.games
+
+    print(json.dumps(output, indent=4, cls=QuakeLogEncoder))
+
 
 if __name__ == "__main__":
     run(parse_arguments())
-
