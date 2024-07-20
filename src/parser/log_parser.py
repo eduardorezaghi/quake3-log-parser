b7f2c489ef72134dcb8943c4faa0fdcf5b2c4719
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-07-19 22:06:05 -0300

    Add missing types to log_parser.py

--- src/parser/log_parser.py
+++ src/parser/log_parser.py
@@ -1,5 +1,6 @@
 import re
 from pathlib import Path
+from typing import Generator
 
 from src.dclasses import QuakeLog
 from src.enums import QuakeDeathCause
@@ -18,12 +19,12 @@ class QuakeLogParser(AbstractLogParser):
         self.games: list[QuakeLog] = []
         self.current_game: QuakeLog | None = None
 
-    def _line_parser(self):
+    def _line_parser(self) -> Generator[str, None, None]:
         with open(self.log_file_path, "r") as log_file:
             for line in log_file:
                 yield line.strip()
 
-    def _group_game_results(self):
+    def _group_game_results(self) -> list[dict]:
         parsed_games = []
         for game in self.games:
             game_result = game.to_dict()
@@ -31,27 +32,27 @@ class QuakeLogParser(AbstractLogParser):
 
         return parsed_games
 
-    def _count_games(self, line):
+    def _count_games(self, line) -> None:
         if re.search(r"InitGame", line):
             self._start_new_game()
         elif re.search(r"ShutdownGame", line) and self.current_game:
             self._end_current_game()
 
-    def _count_players(self, line):
+    def _count_players(self, line) -> None:
         if re.search(r"ClientUserinfoChanged", line):
             # Extract the player name from the log line
             player = re.search(r"n\\([^\\]*)", line).group(1)
             self.current_game.players.add(player)
 
-    def _start_new_game(self):
+    def _start_new_game(self) -> None:
         self.current_game = QuakeLog(game_id=len(self.games) + 1)
 
-    def _end_current_game(self):
+    def _end_current_game(self) -> None:
         if self.current_game:
             self.games.append(self.current_game)
             self.current_game = None
 
-    def parse(self):
+    def parse(self) -> list[dict]:
         for line in self._line_parser():
             self._count_games(line)
             self._count_players(line)
